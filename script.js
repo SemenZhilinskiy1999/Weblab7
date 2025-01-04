d767bf45810d2646a1bec2db4a4fbf0f5254f412
SemenZhilinskiy1999 <99233461+SemenZhilinskiy1999@users.noreply.github.com>
2025-01-04 13:58:49 +0700

    Create script.js

---
+++ script.js
@@ -0,0 +1,15 @@
+console.log("BRUH!")
+
+const a = 5;
+const b = 10;
+
+console.log("a=" + a);
+console.log("b=" + b);
+console.log("a + b = " + (a + b));
+
+console.log(a + b);
+console.log(a - b);
+console.log(a * b);
+console.log(a / b);
+console.log(a % b);
+console.log(a ** b);
